f49edf6c53cd535ded6fe017ae9f6a5941bfab6f
developersharif1919 <developersharif1919@gmail.com>
2023-07-02 23:03:00 +0600

    Complete Work's Section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Intro from '../Intro/Intro';
 import Services from '../Services/Services';
 import Experience from '../Experience/Experience';
+import Works from '../Works/Works';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Intro></Intro>
             <Services></Services>
             <Experience></Experience>
+            <Works></Works>
         </div>
     );
 };
