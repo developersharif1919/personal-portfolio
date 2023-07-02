249155ee5d3c7416bb64325987b3acfe24d90a1d
developersharif1919 <developersharif1919@gmail.com>
2023-07-02 22:47:15 +0600

    Complete Experience Section

---
+++ src/pages/Home/Experience/Experience.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Experience.css'
+
+const Experience = () => {
+    
+    return (
+        <div className="experience" id='experience'>
+            <div className="achievement">
+                {/* darkMode */}
+                <div className="circle" style={{ color:'var(--orange)'}}>1+</div>
+                <span style={{ color: 'black' }}>years </span>
+                <span>Experience</span>
+            </div>
+            <div className="achievement">
+                <div className="circle" style={{ color:'var(--orange)' }}>20+</div>
+                <span style={{ color:'black'}}>completed </span>
+                <span>Projects</span>
+            </div>
+            <div className="achievement">
+                <div className="circle" style={{ color:'var(--orange)' }}>5+</div>
+                <span style={{ color: 'black' }}>companies </span>
+                <span>Work</span>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Intro from '../Intro/Intro';
 import Services from '../Services/Services';
+import Experience from '../Experience/Experience';
 
 const Home = () => {
     return (
         <div>
             <Intro></Intro>
             <Services></Services>
+            <Experience></Experience>
         </div>
     );
 };
